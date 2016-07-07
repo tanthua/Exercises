64dc8395c02af3a3d125f45fca02fd757aa77b71
Tan Hua <tanhua@csus.edu>
2016-07-06 21:21:01 -0700

    testing git

---
+++ Exercises/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+	cout << "Testing Git" << endl;
+	cout << "Testing Git" << endl;
+	cout << "Testing Git" << endl;
+	return 0;
+}
