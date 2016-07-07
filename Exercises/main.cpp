7e2c32f59ab83f464c5e7a80868e620b48d1cbad
Tan Hua <tanhua@csus.edu>
2016-07-06 23:03:46 -0700

    updating new files

--- Exercises/main.cpp
+++ Exercises/main.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
 #include <cstdlib>
+#include <string>
 
 using namespace std;
 
+namespace hua_9_3{
+	void print();
+}
+
 int main() {
-	cout << "Testing Git" << endl;
-	cout << "Testing Git" << endl;
-	cout << "Testing Git" << endl;
+	hua_9_3::print();
+	cout << "Hello Git" << endl;
 	return 0;
+}
+
+namespace hua_9_3 {
+	void print() {
+		string test = "Hello World";
+		cout << test << endl;
+		test.insert(4, 2, '!');
+		cout << test << endl;
+		test.erase(4, 3);
+		cout << test << endl;
+	}
 }
