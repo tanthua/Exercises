1e75a2acc3722e5d7f45502995dbc5c556af4bd3
Tan Hua <tanhua@csus.edu>
2016-07-09 00:09:03 -0700

    finishing 9.6

--- Exercises/main.cpp
+++ Exercises/main.cpp
@@ -5,15 +5,27 @@
 using namespace std;
 
 namespace hua_9_6 {
-
+	unsigned int factorial(unsigned int n);
+	double C(unsigned int n, unsigned int r);
 }
 
 
 
 int main() {
+	cout << hua_9_6::factorial(9) << endl;
+	cout << hua_9_6::C(10, 3) << endl;
 	return 0;
 }
 
 namespace hua_9_6 {
-	
+	unsigned int factorial(unsigned int n) {
+		if (n == 0)
+			return 1;
+		else
+			return n * factorial(n - 1);
+	}
+
+	double C(unsigned int n, unsigned int r) {
+		return (double(factorial(n)) / double(factorial(r) * factorial(n - r)));
+	}
 }
