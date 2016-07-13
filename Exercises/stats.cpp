9807a59d216d7c277618d9734275d8a0f25d3689
thua <thua@LUS-JLHGCS1>
2016-07-13 16:53:26 -0700

    chapter 2

--- Exercises/stats.cpp
+++ Exercises/stats.cpp
@@ -1,6 +1,7 @@
 #include "stats.h"
 #include <cstdlib>
 #include <iostream>
+#include <assert.h>
 
 namespace hua_2C {
 	//CONSTRUCTOR
@@ -36,20 +37,59 @@ namespace hua_2C {
 	}
 
 	double statistician::mean() const {
+		assert(length() > 0);
 		return (sum() / double(length()));
 	}
 
 	double statistician::minimum() const {
+		assert(length() > 0);
 		return tinyest;
 	}
 
 	double statistician::maximum() const {
+		assert(length() > 0);
 		return largest;
 	}
 
 	//NON MEMBER FUNCTIONS
 	statistician operator +(const statistician& s1, const statistician& s2) {
+		statistician s;
+		s.count = s1.length() + s2.length();
+		s.total = s1.sum() + s2.sum();
+		if (s1.minimum() < s2.minimum()) {
+			s.tinyest = s1.minimum();
+		}
+		else {
+			s.tinyest = s2.minimum();
+		}
 
+		if (s1.maximum() > s2.maximum()) {
+			s.largest = s1.maximum();
+		} else {
+			s.largest = s2.maximum();
+		}
+	
+	return s;
 	}
 
+	statistician operator *(double scale, const statistician& s) {
+		statistician _s;
+		_s.count = s.length();
+		_s.total = scale * s.sum();
+		_s.tinyest = scale * s.minimum();
+		_s.largest = scale * s.maximum();
+		return _s;
+	}
+
+	bool operator ==(const statistician& s1, const statistician& s2) {
+		if (s1.length() == s2.length()
+			&& s1.sum() == s2.length()
+			&& s1.minimum() == s2.minimum()
+			&& s1.maximum() == s2.maximum()) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 }
